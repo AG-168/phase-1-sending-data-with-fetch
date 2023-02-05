70b9ce077c86906f45e7b1938250fd5d71a1f3d4
Anson Guo <anson.guo.921104@gmail.com>
2023-02-05 17:13:57 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//     method:"POST",
+//     headers:{
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+// }
+
+// fetch('http://localhost:3000/dogs', configurationObject)
+//     .then(res=>res.json())
+//     .then(obj=>console.log(obj)) 
+//     .catch(error=>{
+//         alert("Have Error");
+//         console.log(error.message)
+//     })
+
+function submitData(inputName,inputEmail) {
+    return fetch(`http://localhost:3000/users`, {
+        method:"POST",
+        headers:{
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body:JSON.stringify({
+            name:inputName,
+            email:inputEmail,
+        })
+    })
+        .then(res=>res.json())
+        .then(data=>appendID(data))
+        .catch(error=>appendError(error))
+
+}
+
+function appendID(data) {
+    const newDiv = document.createElement('div')
+    newDiv.textContent = data.id
+    document.querySelector('body').appendChild(newDiv)
+}
+
+function appendError(error) {
+    const newDiv = document.createElement('div')
+    newDiv.textContent = error.message
+    document.querySelector('body').appendChild(newDiv)
+}
+
+
+
+
